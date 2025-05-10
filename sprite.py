57e33774bea5cf5765bc90f188aaae24b5af9925
SchwarzschildFund <schwarzschildco@gmail.com>
2025-05-10 15:36:12 -0500

    Update sprite.py

--- sprite.py
+++ sprite.py
@@ -2,22 +2,32 @@ import pygame
 from pygame import Rect
 from camera import camera
 
-sprites = []
+sprites_structures = []
 loaded = {}
 
 class Sprite:
-    def __init__(self, image):
+    def __init__(self, image, is_structure):
         if image in loaded:
             self.image = loaded[image]
-        
         else:
             self.image = pygame.image.load(image).convert_alpha()
             loaded[image] = self.image
+        
+        self.is_structure = is_structure
 
-        sprites.append(self)
+
+    def add(self):
+        if self.is_structure == True:
+            sprites_structures.append(self)
+
 
     def delete(self):
-        sprites.remove(self)
+        if self.is_structure == True: 
+            sprites_structures.remove(self)
+
     
     def draw(self, ventana):
+        if self.entity is None:
+            return
+
         ventana.blit(self.image, (self.entity.x - camera.x, self.entity.y - camera.y))
